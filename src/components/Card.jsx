04f677f1ceb519b5dd2e45ab5bbb9bd6d3aac84a
dario <dariopurpi@gmail.com>
2024-09-04 17:48:00 +0200

    Fix modal in navbar

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,20 +4,18 @@ import { Link } from "react-router-dom";
 function Card({ data }) {
   // console.log(data);
   return (
-    <Link to={`/${data?.id}`}>
-      {data && (
-        <div className="card">
-          <div className="cardContent">
-            <img
-              src={`https://image.tmdb.org/t/p/w500/${data?.poster_path}`}
-              alt={data?.overview}
-            />
-            <h3>{data?.title}</h3>
-            <h3>{data?.original_name}</h3>
-          </div>
-        </div>
-      )}
-    </Link>
+    <div className="card">
+      <Link to={`/${data?.id}`}>
+        <span className="cardContent">
+          <img
+            src={`https://image.tmdb.org/t/p/w500/${data?.poster_path}`}
+            alt={data?.overview}
+          />
+          <h3>{data?.title}</h3>
+          <h3>{data?.original_name}</h3>
+        </span>
+      </Link>
+    </div>
   );
 }
 
