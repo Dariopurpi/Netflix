217337f58e59db3cf49751fbed5ecf524a6fe8f4
dario <dariopurpi@gmail.com>
2024-09-04 17:52:57 +0200

    Fix error console

--- src/components/CardsCarousel.jsx
+++ src/components/CardsCarousel.jsx
@@ -9,9 +9,7 @@ function CardsCarousel({ data, sectionTitle }) {
         <h2>{sectionTitle}</h2>
         <div className="cardContainer">
           {data?.map((item) => (
-            <Link key={item.id}>
-              <Card data={item} />
-            </Link>
+            <Card data={item} />
           ))}
         </div>
       </div>
